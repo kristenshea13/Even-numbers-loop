2d2691d8ce781709eac158b794f7f700bcd6389e
Kristen Shea <kstar61@live.com>
2018-08-29 22:11:28 -0500

    even numbers loop with boolean method

--- Even numbers loop/Program.cs
+++ Even numbers loop/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
+
 namespace Even_numbers_loop
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 50; i++)
+            {
+
+                if (IsEven(i))
+                    Console.WriteLine(i);
+
+
+            }
+
+
+            Console.ReadLine();
+           
+            
         }
+
+        private static bool IsEven(int i)
+        {
+            return i % 2 == 0;
+        }
+
+
     }
 }
