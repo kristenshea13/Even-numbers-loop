f51ddd84f5d0a2f8ff35ec125c35904775ba8d26
Kristen Shea <kstar61@live.com>
2018-08-29 22:17:36 -0500

    *

--- Even numbers loop/Program.cs
+++ Even numbers loop/Program.cs
@@ -7,13 +7,11 @@ namespace Even_numbers_loop
     {
         static void Main(string[] args)
         {
+            // wanted my range of integers to be 0 to 50, so i is less than or equal to 50
             for (int i = 0; i <= 50; i++)
             {
-
                 if (IsEven(i))
                     Console.WriteLine(i);
-
-
             }
 
 
@@ -24,6 +22,7 @@ namespace Even_numbers_loop
 
         private static bool IsEven(int i)
         {
+            //returns true if integer is divisible by 2
             return i % 2 == 0;
         }
 
